ba32c62b05f86888103a9c6c5e993a5292c01f52
Philippe Vaucher <philippe.vaucher@gmail.com>
2016-11-12 11:36:14 +0100

    Use plain package.el implementation

--- package-utils.el
+++ package-utils.el
@@ -4,7 +4,7 @@
 ;; URL: https://github.com/Silex/package-utils
 ;; Keywords: package, convenience
 ;; Version: 0.4.1
-;; Package-Requires: ((epl "0.8") (async "1.6"))
+;; Package-Requires: ((async "1.6"))
 
 ;; This file is NOT part of GNU Emacs.
 
@@ -29,41 +29,38 @@
 ;;
 ;;; Code:
 
-(require 'epl)
+(defmacro package-utils-with-packages-list (packages &rest body)
+  "Evaluate BODY inside a `package-list-package' buffer.
+
+PACKAGES is the second argument to `package-menu--generate'."
+  (declare (indent 1))
+  `(with-temp-buffer
+     (package-menu-mode)
+     (package-menu--generate nil ,packages)
+     ,@body))
 
 (defun package-utils-upgradable-packages ()
   "Return the list of upgradable packages as a list of symbols."
-  (mapcar #'epl-package-name (epl-outdated-packages)))
+  (package-utils-with-packages-list t
+    (mapcar #'car (package-menu--find-upgrades))))
 
 (defun package-utils-installed-packages ()
   "Return the list of installed packages as a list of symbols."
-  (mapcar #'epl-package-name (epl-installed-packages)))
-
-(defun package-utils-has-upgradable-packages-p ()
-  "Returns true if there are packages to upgrade, nil otherwise."
-  (not (null (package-utils-upgradable-packages))))
+  (reverse (mapcar #'car package-alist)))
 
 (defun package-utils-read-upgradable-package ()
   "Read the name of a package to upgrade."
-  (completing-read "Upgrade package: "
-                   (mapcar #'symbol-name (package-utils-upgradable-packages))
-                   nil
-                   'require-match))
+  (intern (completing-read "Upgrade package: "
+                           (mapcar #'symbol-name (package-utils-upgradable-packages))
+                           nil
+                           'require-match)))
 
 (defun package-utils-upgradable-p (name)
-  "Returns true if NAME can be upgraded, nil otherwise.
-
-NAME can be a string or a symbol."
-  (unless (symbolp name)
-    (setq name (intern name)))
+  "Returns true if NAME can be upgraded, nil otherwise."
   (not (null (member name (package-utils-upgradable-packages)))))
 
 (defun package-utils-installed-p (name)
-  "Returns true if NAME is installed, nil otherwise.
-
-NAME can be a string or a symbol."
-  (unless (symbolp name)
-    (setq name (intern name)))
+  "Returns true if NAME is installed, nil otherwise."
   (not (null (member name (package-utils-installed-packages)))))
 
 ;;;###autoload
@@ -72,23 +69,28 @@ NAME can be a string or a symbol."
 
 With prefix argument NO-FETCH, do not call `package-refresh-contents'."
   (interactive "P")
-  (package-utils-upgrade-all current-prefix-arg t))
+  (unless no-fetch
+    (package-refresh-contents))
+  (let ((packages (package-utils-upgradable-packages)))
+    (if (null packages)
+        (message "All packages are already up to date.")
+      (message "Upgradable packages: %s" (mapconcat #'symbol-name packages ", ")))))
 
 ;;;###autoload
-(defun package-utils-upgrade-all (&optional no-fetch dry-run)
+(defun package-utils-upgrade-all (&optional no-fetch)
   "Upgrade all packages that can be upgraded.
 
-With prefix argument NO-FETCH, do not call `package-refresh-contents'.
-When DRY-RUN is true, only display what packages would be upgraded."
+With prefix argument NO-FETCH, do not call `package-refresh-contents'."
   (interactive "P")
   (unless no-fetch
     (package-refresh-contents))
-  (if (package-utils-has-upgradable-packages-p)
-      (let ((packages (package-utils-upgradable-packages)))
-        (unless dry-run
-          (epl-upgrade))
-        (message "%s packages: %s" (if dry-run "Upgradable" "Upgraded") (mapconcat 'symbol-name packages ", ")))
-    (message "All packages are already up to date.")))
+  (let ((packages (package-utils-upgradable-packages)))
+    (if (null packages)
+        (message "All packages are already up to date.")
+      (package-utils-with-packages-list t
+        (package-menu-mark-upgrades)
+        (package-menu-execute t))
+      (message "Upgraded packages: %s" (mapconcat 'symbol-name packages ", ")))))
 
 ;;;###autoload
 (defun package-utils-upgrade-all-no-fetch ()
@@ -100,8 +102,6 @@ When DRY-RUN is true, only display what packages would be upgraded."
 (defun package-utils-upgrade-by-name (name &optional no-fetch)
   "Upgrade the package NAME.
 
-NAME can be a string or a symbol.
-
 With prefix argument NO-FETCH, do not call `package-refresh-contents'."
   (interactive
    (progn
@@ -109,34 +109,26 @@ With prefix argument NO-FETCH, do not call `package-refresh-contents'."
        (package-refresh-contents))
      (list (package-utils-read-upgradable-package)
            current-prefix-arg)))
-  (unless (symbolp name)
-    (setq name (intern name)))
-  (unless (package-utils-upgradable-p name)
-    (error "Package \"%s\" not found in the list of upgradable packages" name))
-  (epl-upgrade (epl-find-installed-packages name))
+  (package-utils-with-packages-list (list name)
+    (package-menu-mark-upgrades)
+    (package-menu-execute t))
   (message "Package \"%s\" was upgraded." name))
 
 ;;;###autoload
 (defun package-utils-upgrade-by-name-no-fetch (name)
-  "Upgrade the package NAME, without calling `package-refresh-contents' first.
-
-NAME can be a string or a symbol."
+  "Upgrade the package NAME, without calling `package-refresh-contents' first."
   (interactive (list (package-utils-read-upgradable-package)))
   (package-utils-upgrade-by-name name t))
 
 ;;;###autoload
 (defun package-utils-remove-by-name (name)
-  "Uninstall the package NAME.
-
-NAME can be a string or a symbol."
+  "Uninstall the package NAME."
   (interactive
-   (list (completing-read "Remove package: "
-                          (mapcar #'symbol-name (package-utils-installed-packages))
-                          nil
-                          'require-match)))
-  (unless (symbolp name)
-    (setq name (intern name)))
-  (epl-package-delete (car (epl-find-installed-packages name))))
+   (list (intern (completing-read "Remove package: "
+                                  (mapcar #'symbol-name (package-utils-installed-packages))
+                                  nil
+                                  'require-match))))
+  (package-delete (cadr (assoc name package-alist))))
 
 ;;;###autoload
 (defun package-utils-install-async (package)
