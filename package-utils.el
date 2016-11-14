e37d38b3c94ac39443f0e449f4112b654b6a8fd1
Philippe Vaucher <philippe.vaucher@gmail.com>
2016-11-14 22:13:17 +0100

    Version 0.4.2
    - Refactor using plain package.el implementation.
    - Remove package-utils-list-packages-async.
    - Improve package-utils-install-async interactive spec.

--- package-utils.el
+++ package-utils.el
@@ -3,7 +3,7 @@
 ;; Author: Philippe Vaucher <philippe.vaucher@gmail.com>
 ;; URL: https://github.com/Silex/package-utils
 ;; Keywords: package, convenience
-;; Version: 0.4.1
+;; Version: 0.4.2
 ;; Package-Requires: ((async "1.6"))
 
 ;; This file is NOT part of GNU Emacs.
